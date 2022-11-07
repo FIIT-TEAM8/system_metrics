c194e0e5bc254cde8600e8758b3c900c8c7a2473
Dominik Horvath <dom.horvath17@gmail.com>
2022-11-07 20:27:41 +0100

    daily metrics

--- targets/scraping/routes.py
+++ targets/scraping/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request
 from .recount import Counter
+from db_connector import Database
 
 
 scraping_route = Blueprint("scraping", __name__, url_prefix="/scraping")
@@ -17,7 +18,29 @@ def recount():
     else:
         return "Finished with exception :(", 500
 
-@scraping_route.route("/update", methods=["POST"])
+@scraping_route.route("/daily", methods=["POST"])
 def update_daily():
-    return "bruh"
+    new = request.get_json()
+    scraping_date = new["scraping_date"].split(",")[0]
+    db = Database.get_database_metrics()
+    if db["daily_stats"].count_documents({"date": scraping_date}) == 0:
+        db["daily_stats"].insert_one({
+            "date": scraping_date,
+            "daily_total_elastic": 0,
+            "daily_total_mongo": 0,
+            "daily_total_seconds": 0,
+            "locales": []
+        })
+    old = db["daily_stats"].find_one({"date": scraping_date})
+    old["locales"].append(new)
+    db["daily_stats"].update_one({"date": scraping_date}, {"$set":{
+        "daily_total_elastic": old["daily_total_elastic"] + new["elastic_inserts"],
+        "daily_total_mongo": old["daily_total_mongo"] + new["mongo_inserts"],
+        "daily_total_seconds": old["daily_total_seconds"] + new["elapsed_time_seconds"],
+        "locales": old["locales"]
+    }})
+    
+    return "Created", 201
+
+
 
